6f6209e926cee4f2dfdfc161ab2a35e78590a6c0
Marco <marco.lavacchielli@davinci.edu.ar>
2023-08-20 19:21:27 -0300

    El personaje rota con el stick derecho

---
+++ Assets/Scripts/Rotation.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    [SerializeField] private Player playerInput;
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private Vector3 playerVelocity;
+    [SerializeField] private bool groundedPlayer;
+    [SerializeField] private float playerSpeed = 2.0f;
+    [SerializeField] private float jumpHeight = 1.0f;
+    [SerializeField] private float gravityValue = -9.81f;
+
+    private void Awake()
+    {
+        playerInput = new Player();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnEnable()
+    {
+        playerInput.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerInput.Disable();
+    }
+
+    private void FixedUpdate()
+    {
+        groundedPlayer = Physics.Raycast(transform.position, Vector3.down, 0.1f);
+
+        Vector2 movementInput = playerInput.PlayerMain.Look.ReadValue<Vector2>();
+        Vector3 move = new Vector3(movementInput.x, 0f, movementInput.y).normalized;
+
+        // Rotamos al jugador hacia la dirección del movimiento.
+        if (move != Vector3.zero)
+        {
+            Quaternion newRotation = Quaternion.LookRotation(move);
+            rb.MoveRotation(newRotation);
+        }
+
+        /*if (groundedPlayer && playerInput.PlayerMain.Jump.triggered)
+        {
+            rb.AddForce(Vector3.up * Mathf.Sqrt(jumpHeight * -2f * gravityValue), ForceMode.VelocityChange);
+        }*/
+
+        // Aplicamos la gravedad.
+        rb.AddForce(Vector3.up * gravityValue * Time.fixedDeltaTime, ForceMode.VelocityChange);
+    }
+}
