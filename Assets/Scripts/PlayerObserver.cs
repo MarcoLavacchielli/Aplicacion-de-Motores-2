bb080164f4b420f05b185e23357a851d90bbd11a
Santiago Sanchez <santiago.sanchez@davinci.edu.ar>
2023-09-01 20:57:03 -0300

    El mensaje de HandleEnemy suma 2 veces (se puede disimular)

--- Assets/Scripts/PlayerObserver.cs
+++ Assets/Scripts/PlayerObserver.cs
@@ -4,17 +4,21 @@ using UnityEngine;
 
 public class PlayerObserver : MonoBehaviour
 {
+    private HashSet<IEnemy> observedEnemies = new HashSet<IEnemy>();
+
     private void Start()
     {
         MonoBehaviour[] monoBehaviours = FindObjectsOfType<MonoBehaviour>();
         foreach (var monoBehaviour in monoBehaviours)
         {
-            if (monoBehaviour is IEnemy enemy)
+            if (monoBehaviour is IEnemy enemy && !observedEnemies.Contains(enemy))
             {
+                observedEnemies.Add(enemy);
                 enemy.OnEnemyDeath += HandleEnemyDeath;
             }
         }
     }
+
     private void HandleEnemyDeath()
     {
         // Maneja la notificación de la muerte del enemigo aquí.
