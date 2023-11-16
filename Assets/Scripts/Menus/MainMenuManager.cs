9fd7776b6b8424fcf81d7dd54d247dc26d63db97
Santiago Sanchez <santiago.sanchez@davinci.edu.ar>
2023-11-16 16:05:25 -0300

    maomeno

--- Assets/Scripts/Menus/MainMenuManager.cs
+++ Assets/Scripts/Menus/MainMenuManager.cs
@@ -2,86 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class MainMenuManager : MonoBehaviour
 {
-
-    /*[SerializeField] private GameObject initialScreenPanel;
-    [SerializeField] private GameObject titleScreenPanel;
-    [SerializeField] private GameObject settingsPanel;
-    [SerializeField] private GameObject levelSelectorPanel;
-    [SerializeField] private GameObject storePanel;*/
-
-    [SerializeField] private GameObject[] panels;
-
-    //[SerializeField] private JsonSaveGameManager saveGameManager;
-    [SerializeField] StaminaManager staminaManager;
-    [SerializeField] private GameObject imageStamina;
-    [SerializeField] private float shakeDuration = 0.5f;
-    [SerializeField] private  float intensityShake = 0.1f;
+    private ScreenManager screenManager;
 
     void Awake()
     {
-        staminaManager = FindObjectOfType<StaminaManager>();
+        screenManager = FindObjectOfType<ScreenManager>();
     }
 
-    public void SwitchPanel(int index)  //Setea por indice, el panel a activar
+    public void SwitchPanel(int index)
     {
-        if (index >= 0 && index < panels.Length)
-        {
-            panels[index].SetActive(!panels[index].activeSelf);
-            AudioManager.Instance.PlaySFX(0); //hace Play del sonido del boton (actualmente, indice 0)
-        }
-    }
-
-    public void TogglePanel(GameObject panelToToggle) //setea por editor, el panel a desactivar
-    {
-        panelToToggle.SetActive(!panelToToggle.activeSelf);
-    }
-
-    public void loadscene(string scenename) //carga una escena especifica por nombre
-    {
-        SceneManager.LoadScene(scenename);    //sin estamina
-    }
-
-    public void QuitGame() //Cierra el juego, gracias por jugar o pispear
-    {
-        Application.Quit();
-    }
-
-    public void loadLevel(string level)
-    {
-        if (staminaManager.HasEnoughStamina(1))
-        {
-            staminaManager.UseStamina(1);
-            SceneManager.LoadScene(level);
-        }
-        else
-        {
-            StartCoroutine(ShakeImagen());
-        }
-    }
-
-    IEnumerator ShakeImagen()
-    {
-        Vector3 posicionInicial = imageStamina.transform.position;
-        float tiempoInicio = Time.time;
-
-        while (Time.time - tiempoInicio < shakeDuration)
-        {
-            float offsetX = Random.Range(-intensityShake, intensityShake);
-            float offsetY = Random.Range(-intensityShake, intensityShake);
-
-            imageStamina.transform.position = new Vector3(posicionInicial.x + offsetX, posicionInicial.y + offsetY, posicionInicial.z);
-
-            yield return null;
-        }
-        imageStamina.transform.position = posicionInicial;
-    }
-
-    /*public void PlaySfx()
-    {
+        screenManager.SwitchPanel(index);
         AudioManager.Instance.PlaySFX(0);
-    }*/
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        screenManager.LoadScene(sceneName);
+    }
+
+    public void QuitGame()
+    {
+        screenManager.QuitGame();
+    }
+
+    public void LoadLevel(string level)
+    {
+        screenManager.LoadLevel(level);
+    }
+
 }
---
+++ Assets/Scripts/Menus/ScreenManager.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScreenManager : MonoBehaviour
+{
+    [SerializeField] private GameObject[] panels;
+    [SerializeField] private StaminaManager staminaManager;
+    [SerializeField] private GameObject imageStamina;
+    [SerializeField] private float shakeDuration = 0.5f;
+    [SerializeField] private float intensityShake = 0.1f;
+
+    private int currentPanelIndex = -1;
+
+    void Awake()
+    {
+        staminaManager = FindObjectOfType<StaminaManager>();
+        ShowPanel(currentPanelIndex); // Mostrar un panel al inicio (puedes cambiar esto según tus necesidades)
+    }
+
+    public void SwitchPanel(int index)
+    {
+        if (index >= 0 && index < panels.Length && index != currentPanelIndex)
+        {
+            HideCurrentPanel();
+            currentPanelIndex = index;
+            ShowPanel(index);
+            AudioManager.Instance.PlaySFX(0); // Reproducir sonido del botón (actualmente, índice 0)
+        }
+    }
+
+    private void ShowPanel(int index)
+    {
+        if (index >= 0 && index < panels.Length)
+        {
+            panels[index].SetActive(true);
+        }
+    }
+
+    private void HideCurrentPanel()
+    {
+        if (currentPanelIndex >= 0 && currentPanelIndex < panels.Length)
+        {
+            panels[currentPanelIndex].SetActive(false);
+        }
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void LoadLevel(string level)
+    {
+        if (staminaManager.HasEnoughStamina(1))
+        {
+            staminaManager.UseStamina(1);
+            SceneManager.LoadScene(level);
+        }
+        else
+        {
+            StartCoroutine(ShakeImage());
+        }
+    }
+
+    public void panelNotClose(int index)
+    {
+        panels[index].SetActive(false);
+    }
+
+    IEnumerator ShakeImage()
+    {
+        Vector3 initialPosition = imageStamina.transform.position;
+        float startTime = Time.time;
+
+        while (Time.time - startTime < shakeDuration)
+        {
+            float offsetX = Random.Range(-intensityShake, intensityShake);
+            float offsetY = Random.Range(-intensityShake, intensityShake);
+
+            imageStamina.transform.position = new Vector3(initialPosition.x + offsetX, initialPosition.y + offsetY, initialPosition.z);
+
+            yield return null;
+        }
+
+        imageStamina.transform.position = initialPosition;
+    }
+}
