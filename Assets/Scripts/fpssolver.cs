142953f3ffa3bf6787aeffa116d99d7054c8afce
Santiago Sanchez <santiago.sanchez@davinci.edu.ar>
2023-12-15 11:49:41 -0300

    fps asi va mas fluido

---
+++ Assets/Scripts/fpssolver.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fpssolver : MonoBehaviour
+{
+
+    private void Awake()
+    {
+        Application.targetFrameRate = 144;
+    }
+}
